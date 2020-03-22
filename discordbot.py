cfde5067c5922768ea9797f825b378fbb726c94a
root <root@localhost.localdomain>
2020-03-22 19:11:38 +0900

    WIP

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='#')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
